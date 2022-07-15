18d3f1e9f988035f75c6d5533c74453f4a5fdc56
carlos77E <carlitosmoran09@gmail.com>
2022-07-15 08:11:35 -0300

    se agrega el proyecto

---
+++ src/main/java/com/mycompany/ejercicio1s2/Ejercicio1S2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio1s2;
+
+/**
+ *
+ * @author USER
+ */
+public class Ejercicio1S2 {
+
+   public static void main(String[] args) {
+         System.out.println("Numeros del 1 al 100: ");                                                            
+         int i=1;
+         while(i<=100) {
+                   System.out.println(i);                                                                         
+                   i++;
+         }
+    }
+}
